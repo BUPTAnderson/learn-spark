4c9e7c1247e3bd3cf031889465541867a674b5d7
BUPTAnderson <229546106@qq.com>
2017-09-18 09:32:55 +0800

    update jdbcOperation

--- src/main/scala/org/training/spark/sql/JDBCOperation.scala
+++ src/main/scala/org/training/spark/sql/JDBCOperation.scala
@@ -11,13 +11,16 @@ import org.apache.spark.sql.SparkSession
   * export SPARK_CLASSPATH=$SPARK_CLASSPATH:/home/anderson/.m2/repository/mysql/mysql-connector-java/5.1.35/mysql-connector-java-5.1.35.jar
   * 则./spark-submit 不能使用--driver-class-path
   * 如果没有配置 则需要在启动命令中指定 --driver-class-path
-  *  cat jdbcoperation.sh
-  /home/anderson/GitHub/spark-2.2.0-bin-hadoop2.6/bin/spark-submit --driver-class-path /home/anderson/.m2/repository/mysql/mysql-connector-java/5.1.35/mysql-connector-java-5.1.35.jar \
-  --class org.training.spark.sql.JDBSOperation \
-  --master yarn \
-  --deploy-mode client \
-  --executor-memory 512m \
-  --num-executors 1 \
+  *
+  * 启动脚本:
+  cat jdbcoperation.sh
+  /home/anderson/GitHub/spark-2.2.0-bin-hadoop2.6/bin/spark-submit \
+    --driver-class-path /home/anderson/.m2/repository/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar \
+    --class org.training.spark.sql.JDBCOperation \
+    --master yarn \
+    --deploy-mode client \
+    --executor-memory 512m \
+    --num-executors 1 \
   /home/anderson/gitJD/sparksql/jdbcoperation.jar \
 
   */
@@ -27,12 +30,14 @@ object JDBCOperation {
     val spark = SparkSession
       .builder()
       .config(conf)
+      .config("spark.driver.extraClassPath", "/home/anderson/.m2/repository/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar")
+      .config("spark.executor.extraClassPath", "/home/anderson/.m2/repository/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar")
       .getOrCreate()
 
     val properties = new Properties()
     properties.put("user", "root")
     properties.put("password", "root")
-    val studentDF = spark.read.jdbc("jdbc:mysql://localhost:3306/test", "student", properties)
+    val studentDF = spark.read.jdbc("jdbc:mysql://localhost:3306", "test.student", properties)
     studentDF.show()
   }
 }
